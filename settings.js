e37d12b5f05f37d718c9f615da1fffb2e111faf3
cyber-kid <102310990+CYBERXKID@users.noreply.github.com>
2022-06-30 10:05:12 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //═══════════════════════════════════════════════════════//
 //
-//                              𝙶𝙾𝙹𝙾-𝚂𝙰𝚃𝙾𝚁𝚄 𝓫𝔂 𝓷𝓮𝔁𝓾𝓼𝓝𝔀
+//                              ALEXA-QUEEN 𝐛𝐲 𝐜𝐲𝐛𝐞𝐫𝐱𝐤𝐢𝐝
 //𝙰𝙳𝙾𝙿𝚃𝙴𝙳 𝙵𝚁𝙾𝙼  𝚂𝙲𝚁𝙸𝙿𝚃 𝙾𝙵 𝙲𝙷𝙴𝙴𝙼𝚂𝙱𝙾𝚃 𝚅2 𝙱𝚈 𝙳𝙶𝚇𝚎𝚘𝚗 
 //
 //════════════════════════════//
@@ -22,15 +22,15 @@ global.APIKeys = {
 global.owner = ['Your Number']
 global.premium = ['Your Number']
 global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
-global.ig = 'https://github.com/nexusNw'
+global.ownername = '🐦CYBERXKID'
+global.botname = 'ALEXA-QUEEN'
+global.footer = '©CYBERXKIDnc.'
+global.ig = 'https://github.com/CYBERXKID'
 global.region = 'India, South India, Kerala'
-global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.sc = 'https://github.com/Alexa-Queen-MD'
+global.myweb = 'https://youtube.com/channel/UCKW8EUxAo6A7RKhc35H54wg'
+global.packname = 'ALEXA-QUEEN'
+global.author = '🐦cyberxkid'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
